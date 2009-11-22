72946fe40b2f67ee5595ae4200489c0762d802b4
Joshua Peek <josh@joshpeek.com>
2009-11-21 17:47:17 -0800

    clean up parse

--- lib/reginald.rb
+++ lib/reginald.rb
@@ -8,55 +8,70 @@ module Reginald
   autoload :Group, 'reginald/group'
   autoload :Parser, 'reginald/parser'
 
-  begin
-    eval('/(?<foo>.*)/').named_captures
+  class << self
+    begin
+      eval('/(?<foo>.*)/').named_captures
 
-    def self.regexp_supports_named_captures?
-      true
-    end
-  rescue SyntaxError, NoMethodError
-    def self.regexp_supports_named_captures?
-      false
-    end
-  end
-
-  def self.parse(regexp)
-    # TODO: The parser should be aware of extended expressions
-    # instead of having to sanitize them before.
-    if regexp.options & Regexp::EXTENDED != 0
-      source = regexp.source
-      source.gsub!(/#.+$/, '')
-      source.gsub!(/\s+/, '')
-      source.gsub!(/\\\//, '/')
-      regexp = Regexp.compile(source)
-    end
-
-    parser = Parser.new
-    expression = parser.scan_str(regexp.source)
-    expression.ignorecase = regexp.casefold?
-
-    capture_index = 0
-    tag_captures = Proc.new do |expr|
-      expr.each do |atom|
-        if atom.is_a?(Group)
-          if atom.capture
-            atom.index = capture_index
-            capture_index += 1
-          end
-          tag_captures.call(atom)
-        elsif atom.is_a?(Expression)
-          tag_captures.call(atom)
-        end
+      def regexp_supports_named_captures?
+        true
+      end
+    rescue SyntaxError, NoMethodError
+      def regexp_supports_named_captures?
+        false
       end
     end
-    tag_captures.call(expression)
 
-    expression
-  end
+    def parse(regexp)
+      regexp = strip_extended_whitespace_and_comments(regexp)
 
-  def self.compile(source)
-    regexp = Regexp.compile(source)
-    expression = parse(regexp)
-    Regexp.compile(expression.to_s_without_options, expression.options)
+      parser = Parser.new
+      expression = parser.scan_str(regexp.source)
+
+      expression.ignorecase = regexp.casefold?
+
+      tag_captures!(expression)
+
+      expression
+    end
+
+    def compile(source)
+      regexp = Regexp.compile(source)
+      expression = parse(regexp)
+      Regexp.compile(expression.to_s_without_options, expression.options)
+    end
+
+    private
+      # TODO: The parser should be aware of extended expressions
+      # instead of having to sanitize them before.
+      def strip_extended_whitespace_and_comments(regexp)
+        if regexp.options & Regexp::EXTENDED != 0
+          source = regexp.source
+          source.gsub!(/#.+$/, '')
+          source.gsub!(/\s+/, '')
+          source.gsub!(/\\\//, '/')
+          regexp = Regexp.compile(source)
+        else
+          regexp
+        end
+      end
+
+      # TODO: The parser should tag capture indexes
+      def tag_captures!(expression)
+        capture_index = 0
+        iterator = Proc.new do |expr|
+          expr.each do |atom|
+            if atom.is_a?(Group)
+              if atom.capture
+                atom.index = capture_index
+                capture_index += 1
+              end
+              iterator.call(atom)
+            elsif atom.is_a?(Expression)
+              iterator.call(atom)
+            end
+          end
+        end
+        iterator.call(expression)
+      end
   end
 end
