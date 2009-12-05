d172c2f62b93f5c1d7b37d4b1691271eb9bfda30
Joshua Peek <josh@joshpeek.com>
2009-12-05 10:44:29 -0600

    benchmark results

--- benchmark/bm_parse.rb
+++ benchmark/bm_parse.rb
@@ -8,3 +8,8 @@ Benchmark.bmbm do |x|
   x.report('foo(/bar)?')        { TIMES.times { Reginald.parse(/foo(\/bar)?/) } }
   x.report('foo(/bar(/baz)?)?') { TIMES.times { Reginald.parse(/foo(\/bar(\/baz)?)?/) } }
 end
+
+#                         user     system      total        real
+# foo                 1.420000   0.020000   1.440000 (  1.440483)
+# foo(/bar)?          3.790000   0.050000   3.840000 (  3.839614)
+# foo(/bar(/baz)?)?   6.190000   0.070000   6.260000 (  6.262343)
